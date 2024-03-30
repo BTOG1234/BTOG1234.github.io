2f3434f08745b887d06923454b7057fd10b7c06b
BTOG1234 <165496230+BTOG1234@users.noreply.github.com>
2024-03-31 02:59:52 +1100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+function startGame() {
+    window.location.href = "game.html";
+}
+
+function showInstructions() {
+    window.location.href = "instructions.html";
+}
+
+function showCredits() {
+    window.location.href = "credits.html";
+}
